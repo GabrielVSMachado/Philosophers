fe2cc3cf4ccac970bf8e616b8516f579d23e51ca
gvitor-s <gvitor-s@student.42sp.org.com>
2022-04-20 20:07:45 -0300

    Feat: function to create hierachy of mutex

--- philo/src/utils_actions.c
+++ philo/src/utils_actions.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 16:04:25 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/04/20 17:33:08 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/04/20 19:54:43 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
+#include "philo.h"
 
 int	get_current_time(void)
 {
@@ -32,4 +33,38 @@ void	get_fork_in_position(pthread_mutex_t *forks, int position, int seat)
 void	leave_fork(pthread_mutex_t *forks, int position)
 {
 	pthread_mutex_unlock(&forks[position]);
+}
+
+int	wait_until_odd_end(struct s_table *table)
+{
+	static pthread_mutex_t	_ = PTHREAD_MUTEX_INITIALIZER;
+	int						n_phls;
+	int						seat;
+
+	seat = table->philosophers->seat;
+	n_phls = table->n_philophers;
+	while (pthread_mutex_lock(&_), table->odd_time[(seat - 1) % (n_phls / 2)])
+	{
+		pthread_mutex_unlock(&_);
+		if (get_current_time() - table->philosophers->thinking >= table->die)
+			return (1);
+	}
+	return (0);
+}
+
+int	wait_until_even_end(struct s_table *table)
+{
+	static pthread_mutex_t	_ = PTHREAD_MUTEX_INITIALIZER;
+	int						n_phls;
+	int						seat;
+
+	seat = table->philosophers->seat;
+	n_phls = table->n_philophers;
+	while (pthread_mutex_lock(&_), !table->odd_time[(seat - 1) % (n_phls / 2)])
+	{
+		pthread_mutex_unlock(&_);
+		if (get_current_time() - table->philosophers->thinking >= table->die)
+			return (1);
+	}
+	return (0);
 }
--- philo/src/utils_actions.h
+++ philo/src/utils_actions.h
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 16:07:22 by gvitor-s          #+#    #+#             */
-/*   Updated: 2022/04/20 18:46:47 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2022/04/20 19:55:28 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 # define UTILS_ACTIONS_H
 
 # include <pthread.h>
+# include "philo.h"
 
 int		get_current_time(void);
 void	leave_fork(pthread_mutex_t *forks, int position);
 void	get_fork_in_position(pthread_mutex_t *forks, int position, int seat);
+int		wait_until_odd_end(struct s_table *table);
+int		wait_until_even_end(struct s_table *table);
 #endif
